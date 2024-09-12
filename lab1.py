5f10e4a2612a5509feb5fc70f10c93a207ad948c
ApsilonXi <volkov.volkova2000@mail.ru>
2024-09-12 21:44:25 +0300

    окончательный вариаент

--- lab1.py
+++ lab1.py
@@ -1,3 +1,19 @@
+def help_def_no_key(symbol):
+    new = ''
+    for row in range(6):
+        for colmn in range(6):
+            if polibius_alph[row][colmn] == symbol.upper():
+                new = f'{row}{colmn}'
+                return new
+
+def help_def_with_key(symbol, key):
+    new = ''
+    for row in range(6):
+        for colmn in range(6):
+            if polibius_alph[row][colmn] == symbol.upper():
+                new = f'{key[row]}{key[colmn]}'
+                return new
+
 def encrypt(text, key):
     match key:
         case '0':
@@ -5,38 +21,42 @@ def encrypt(text, key):
                 new_text = ''
                 for i in f.readlines():
                     for j in i:
-                        for row in range(6):
-                            for colmn in range(6):
-                                if polibius_alph[row][colmn] == j.upper():
-                                    new_text += f'{row}{colmn}'
-                                    continue
-                                
-
+                        new_text += help_def_no_key(j)
             with open('c:\EmilyVolkova\VUZ\Shifrovanie\encrypted_mumu.txt', 'w', encoding='utf-8') as f2:
                 f2.write(new_text)
         case any:
-            pass
+            with open(text, 'r', encoding='utf-8') as f:
+                new_text = ''
+                for i in f.readlines():
+                    for j in i:
+                        new_text += help_def_with_key(j, key)
+            with open('c:\EmilyVolkova\VUZ\Shifrovanie\encrypted_mumu.txt', 'w', encoding='utf-8') as f2:
+                f2.write(new_text)
 
 def decrypt(text, key):
     match key:
         case '0':
             with open(text, 'r', encoding='utf-8') as f:
-                enc_text = f.readlines()
-                print(enc_text)
+                enc_text = f.readlines()[0]
                 new_text = ''
                 for i in range(0, len(enc_text), 2):
-                    print(i, enc_text[i], enc_text[i+1])
                     try:
                         new_text += polibius_alph[int(enc_text[i])][int(enc_text[i+1])]
                     except IndexError:
                         break
-
             with open('c:\EmilyVolkova\VUZ\Shifrovanie\decrypted_mumu.txt', 'w', encoding='utf-8') as f2:
                 f2.write(new_text)
-
-                    
         case any:
-            pass
+            with open(text, 'r', encoding='utf-8') as f:
+                enc_text = f.readlines()[0]
+                new_text = ''
+                for i in range(0, len(enc_text), 2):
+                    try:
+                        new_text += polibius_alph[key.index(enc_text[i])][key.index(enc_text[i+1])]
+                    except IndexError:
+                        break
+            with open('c:\EmilyVolkova\VUZ\Shifrovanie\decrypted_mumu.txt', 'w', encoding='utf-8') as f2:
+                f2.write(new_text)
 
 polibius_alph = [
     ['А', 'Б', 'В', 'Г', 'Д', 'Е'],
@@ -44,7 +64,7 @@ polibius_alph = [
     ['Л', 'М', 'Н', 'О', 'П', 'Р'],
     ['С', 'Т', 'У', 'Ф', 'Х', 'Ц'],
     ['Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь'],
-    ['Э', 'Ю', 'Я', '.', ',', '?']
+    ['Э', 'Ю', 'Я', ' ', '.', ',']
 ]
 
 path_en = 'c:/EmilyVolkova/VUZ/Shifrovanie/mumu.txt'
@@ -55,11 +75,17 @@ while True:
     match choice:
         case '1':
             key = input('Ключ: ')
-            encrypt(path_en, key)
+            if (key != 0) or (len(key) == 6):
+                encrypt(path_en, key)
+            else:
+                print('Неверный ключ')
             break
         case '2':
             key = input('Ключ: ')
-            decrypt(path_dec, key)
+            if (key != 0) or (len(key) == 6):
+                decrypt(path_dec, key)
+            else:
+                print('Неверный ключ')
             break
         case any:
             print("\nВведите верный вариант\n")
